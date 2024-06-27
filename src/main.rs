6e91dcfa88a022b30470997e0b118f5e2990eea8
Anton Eriksson <antoneriksson978@gmail.com>
2024-06-27 23:17:19 +0200

    add names.txt and implement bigram count map

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,30 @@
-fn main() {
-    println!("Hello, world!");
+use std::collections::HashMap;
+use std::fs::File;
+use std::io::{self, BufRead, BufReader};
+use std::iter;
+use std::path::Path;
+
+fn main() -> io::Result<()> {
+    let input_path = Path::new("data/names.txt");
+
+    let input_file = File::open(&input_path)?;
+    let reader = BufReader::new(input_file);
+    let words = reader
+        .lines()
+        .map(|line| line.unwrap())
+        .collect::<Vec<String>>();
+
+    let mut bigram_counts = HashMap::<(char, char), i32>::new();
+    for word in words {
+        let chars: Vec<char> = iter::once('S')
+            .chain(word.chars())
+            .chain(iter::once('E'))
+            .collect();
+        for (ch1, ch2) in chars.windows(2).map(|w| (w[0], w[1])) {
+            let bigram = (ch1, ch2);
+            bigram_counts.insert(bigram, bigram_counts.get(&bigram).unwrap_or(&0) + 1);
+        }
+    }
+    println!("{:?}", bigram_counts);
+    Ok(())
 }
