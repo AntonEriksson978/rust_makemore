766fbf355a2fe9b7e6066462140eebe95bdf3308
Anton Eriksson <antoneriksson978@gmail.com>
2024-06-28 23:05:03 +0200

    create a tensor with bigram counts

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,7 @@ use std::io::{self, BufRead, BufReader};
 use std::iter;
 use std::path::Path;
 
-use burn::tensor::Tensor;
+use burn::tensor::{Data, Int, Tensor};
 
 type BE = Candle;
 fn main() -> io::Result<()> {
@@ -16,27 +16,54 @@ fn main() -> io::Result<()> {
     let reader = BufReader::new(input_file);
     let words = reader
         .lines()
-        .take(10)
         .map(|line| line.unwrap())
         .collect::<Vec<String>>();
 
     let device = CandleDevice::default();
-    let a = Tensor::<BE, 2>::full([28, 28], 0, &device);
+    let alphabet = [
+        (0, '.'),
+        (1, 'a'),
+        (2, 'b'),
+        (3, 'c'),
+        (4, 'd'),
+        (5, 'e'),
+        (6, 'f'),
+        (7, 'g'),
+        (8, 'h'),
+        (9, 'i'),
+        (10, 'j'),
+        (11, 'k'),
+        (12, 'l'),
+        (13, 'm'),
+        (14, 'n'),
+        (15, 'o'),
+        (16, 'p'),
+        (17, 'q'),
+        (18, 'r'),
+        (19, 's'),
+        (20, 't'),
+        (21, 'u'),
+        (22, 'v'),
+        (23, 'w'),
+        (24, 'x'),
+        (25, 'y'),
+        (26, 'z'),
+    ];
 
-    println!("{}", a);
-
-    let mut bigram_counts = HashMap::<(char, char), i32>::new();
+    let mut arr = [[0.0; 27]; 27];
     for word in words {
-        let chars: Vec<char> = iter::once('S')
+        let chars: Vec<char> = iter::once('.')
             .chain(word.chars())
-            .chain(iter::once('E'))
+            .chain(iter::once('.'))
             .collect();
         let bigrams = chars.windows(2).map(|w| (w[0], w[1]));
         for bigram in bigrams {
-            let count = bigram_counts.get(&bigram).unwrap_or(&0) + 1;
-            bigram_counts.insert(bigram, count);
+            let start = alphabet.iter().find(|(_, c)| *c == bigram.0).unwrap().0 as usize;
+            let end = alphabet.iter().find(|(_, c)| *c == bigram.1).unwrap().0 as usize;
+            arr[start][end] += 1.0;
         }
     }
-    println!("{:?}", bigram_counts);
+    let tensor = Tensor::<BE, 2>::from_floats(arr, &device);
+
     Ok(())
 }
