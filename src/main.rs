ac1824eae5e5776410e787b488581afb4135950c
Anton Eriksson <antoneriksson978@gmail.com>
2024-07-02 21:05:21 +0200

    tensor indexing and log likelihood

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ use std::io::{self, BufRead, BufReader};
 use std::iter;
 use std::path::Path;
 
-use burn::tensor::{Data, Float, Tensor};
+use burn::tensor::Tensor;
 
 type BE = Candle;
 fn main() -> io::Result<()> {
@@ -50,7 +50,7 @@ fn main() -> io::Result<()> {
     ];
 
     let mut bigram_counts = [[0.0; 27]; 27];
-    for word in words {
+    for word in words.clone() {
         let chars: Vec<char> = iter::once('.')
             .chain(word.chars())
             .chain(iter::once('.'))
@@ -65,7 +65,42 @@ fn main() -> io::Result<()> {
 
     let counts_tensor = Tensor::<BE, 2>::from_floats(bigram_counts, &device);
     let prob_tensor = counts_tensor.clone() / counts_tensor.clone().sum_dim(1);
-    println!("{}", prob_tensor);
+    let log_prob_tensor = prob_tensor.clone().log();
+
+    // println!("{}", prob_tensor);
+    // println!("{}", prob_tensor.to_data().shape.num_elements());
+    // println!("{}", prob_tensor.to_data().value[3 + 27 * 4]);
+    // println!("{}", prob_tensor.clone().slice([0..1, 0..1]).to_data());
+    // // println!("{}", prob_tensor.slice([0..1, 0..1]).to_data());
+
+    let mut log_prob = 0.0;
+    for word in words.iter().take(3) {
+        let chars: Vec<char> = iter::once('.')
+            .chain(word.chars())
+            .chain(iter::once('.'))
+            .collect();
+        let bigrams = chars.windows(2).map(|w| (w[0], w[1]));
+        for (ch1, ch2) in bigrams {
+            let ch1_index = alphabet.iter().position(|&(_, c)| c == ch1).unwrap();
+            let ch2_index = alphabet.iter().position(|&(_, c)| c == ch2).unwrap();
+            let prop = prob_tensor
+                .clone()
+                .slice([ch1_index..ch1_index + 1, ch2_index..ch2_index + 1]);
+            println!(
+                "{}{}: {}",
+                alphabet[ch1_index].1,
+                alphabet[ch2_index].1,
+                prop.to_data().value[0]
+            );
+            log_prob += log_prob_tensor
+                .clone()
+                .slice([ch1_index..ch1_index + 1, ch2_index..ch2_index + 1])
+                .to_data()
+                .value[0];
+        }
+        println!("{}: {}", word, log_prob);
+    }
+    println!("Log likelihood: {}", log_prob);
 
     Ok(())
 }
